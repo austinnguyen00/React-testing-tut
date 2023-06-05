3b60f654b5a20e2369e32905fc50c5a7dbbb514e
Austin <ndkhiem3010@gmail.com>
2023-06-05 22:24:46 +0800

    Adding test cases for login form and button

--- src/components/login/Login.test.tsx
+++ src/components/login/Login.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Login from "./Login";
 
 test("username input should be rendered", () => {
@@ -17,4 +17,72 @@ test("button input should be rendered", () => {
 	render(<Login />);
 	const buttonInputEl = screen.getByRole("button");
 	expect(buttonInputEl).toBeInTheDocument();
+});
+
+test("username input should be empty", () => {
+	render(<Login />);
+	const userInputEl = screen.getByPlaceholderText(
+		/username/i,
+	) as HTMLInputElement;
+	expect(userInputEl.value).toBe("");
+});
+
+test("password input should be empty", () => {
+	render(<Login />);
+	const passwordInputEl = screen.getByPlaceholderText(
+		/password/i,
+	) as HTMLInputElement;
+	expect(passwordInputEl.value).toBe("");
+});
+
+test("button should be disabled", () => {
+	render(<Login />);
+	const buttonEl = screen.getByRole("button");
+	expect(buttonEl).toBeDisabled();
+});
+
+test("error message should not be visible", () => {
+	render(<Login />);
+	const errorEl = screen.getByTestId("error");
+	expect(errorEl).not.toBeVisible();
+});
+
+test("username input should change", () => {
+	render(<Login />);
+	const userInputEl = screen.getByPlaceholderText(
+		/username/i,
+	) as HTMLInputElement;
+	const testValue = "test";
+
+	fireEvent.change(userInputEl, { target: { value: testValue } });
+	expect(userInputEl.value).toBe(testValue);
+});
+
+test("password input should change", () => {
+	render(<Login />);
+	const passwordInputEl = screen.getByPlaceholderText(
+		/password/i,
+	) as HTMLInputElement;
+	const testValue = "test";
+
+	fireEvent.change(passwordInputEl, { target: { value: testValue } });
+	expect(passwordInputEl.value).toBe(testValue);
+});
+
+test("button should not be disabled when inputs exist", () => {
+	render(<Login />);
+	const userInputEl = screen.getByPlaceholderText(
+		/username/i,
+	) as HTMLInputElement;
+	const passwordInputEl = screen.getByPlaceholderText(
+		/password/i,
+	) as HTMLInputElement;
+	const buttonEl = screen.getByRole("button");
+
+	const testValue = "test";
+
+	fireEvent.change(userInputEl, { target: { value: testValue } });
+	fireEvent.change(passwordInputEl, { target: { value: testValue } });
+
+	expect(buttonEl).not.toBeDisabled();
 });
--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,18 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+	const [error, setError] = useState<boolean>(false);
+	const [username, setUsername] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
+
+	const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+		event.preventDefault();
+		try {
+		} catch (err) {
+			setError(true);
+		}
+	};
+
 	return (
 		<div className="container">
 			<form>
 				<input
 					type="text"
 					placeholder="username"
+					value={username}
+					onChange={(e) => {
+						setUsername(e.target.value);
+					}}
 				/>
 				<input
 					type="password"
 					placeholder="password"
+					value={password}
+					onChange={(e) => {
+						setPassword(e.target.value);
+					}}
 				/>
-				<button>Login</button>
+				<button
+					disabled={!username || !password}
+					onClick={handleClick}
+				>
+					Login
+				</button>
+				<span
+					data-testid="error"
+					style={{ visibility: error ? "visible" : "hidden" }}
+				>
+					Something went wrong!
+				</span>
 			</form>
 		</div>
 	);
